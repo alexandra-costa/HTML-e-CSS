50f958a6dc2fa1fa887d2a41ca289c8677a8b638
alexandra-costa <alexandraaraujodacosta@gmail.com>
2021-09-23 15:10:51 -0300

    fix: refactoring html and css

---
+++ index.js
@@ -0,0 +1,17 @@
+function foo(e) {
+    const flag = true;
+    const article1 = document.getElementById("art1");
+    const article2 = document.getElementById("art2");
+    
+    if (article1.style.maxWidth === "") {
+      article1.style.maxWidth = "25%";
+      article2.style.maxWidth = "75%";
+      img = "cancel.png";
+    } else {
+      article1.style.maxWidth = "";
+      article2.style.maxWidth = "100%"
+      img = "menu.png";
+    }
+  }
+ 
+        
